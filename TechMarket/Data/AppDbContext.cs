4122be8acd78b3894450a3627742c312cb71f919
Umut Tokgöz <umttkgz@gmail.com>
2024-02-27 17:06:06 +0300

    Category Table Added with Seed Values

--- TechMarket/Data/AppDbContext.cs
+++ TechMarket/Data/AppDbContext.cs
@@ -10,5 +10,16 @@ namespace TechMarket.Data
         {
         }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Telefon", DisplayOrder = 1},
+                new Category { Id = 2, Name = "Bilgisayar", DisplayOrder = 2},
+                new Category { Id = 3, Name = "Televizyon", DisplayOrder = 3}
+                );
+        }
     }
 }
