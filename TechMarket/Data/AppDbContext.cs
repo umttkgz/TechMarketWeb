8454c956be1ef6d8a2018275840c93d59555a67f
Umut Tokgöz <umttkgz@gmail.com>
2024-02-27 16:48:50 +0300

    AppDbContext and Sql Connection Configured

---
+++ TechMarket/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using TechMarket.Models;
+
+namespace TechMarket.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
