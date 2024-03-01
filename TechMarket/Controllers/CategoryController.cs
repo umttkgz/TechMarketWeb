ac410f2c698db58260bf5b800e641b6fb990d3b0
Umut Tokgöz <umttkgz@gmail.com>
2024-03-01 22:42:48 +0300

    Category List Added

--- TechMarket/Controllers/CategoryController.cs
+++ TechMarket/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TechMarket.Data;
+using TechMarket.Models;
 
 namespace TechMarket.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
