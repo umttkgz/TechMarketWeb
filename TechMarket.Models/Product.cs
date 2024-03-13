6bfb4b6f4c7fa09a97422e4bab4d84a1dae68995
Umut Tokgöz <umttkgz@gmail.com>
2024-03-13 15:48:10 +0300

    Product Model Created

---
+++ TechMarket.Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechMarket.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        [Display(Name = "Fiyat")]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+        [Required]
+        public int BrandId { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [Required]
+        public int SubCategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
+        [ForeignKey("SubCategoryId")]
+        [ValidateNever]
+        public SubCategory SubCategory { get; set; }
+        [ForeignKey("BrandId")]
+        [ValidateNever]
+        public Brand Brand { get; set; }
+
+    }
+}
