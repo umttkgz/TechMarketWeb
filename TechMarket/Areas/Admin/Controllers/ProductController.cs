290297b144fa6097eeb333b2834391ca2a042539
Umut Tokgöz <umttkgz@gmail.com>
2024-03-18 01:50:36 +0300

    Handle Image on Update

--- TechMarket/Areas/Admin/Controllers/ProductController.cs
+++ TechMarket/Areas/Admin/Controllers/ProductController.cs
@@ -64,6 +64,18 @@ namespace TechMarket.Areas.Admin.Controllers
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                     string productPath = Path.Combine(wwwRootPath, @"images\product");
 
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        //delete the old image
+                        var oldImagePath =
+                            Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
                     using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
                     {
                         file.CopyTo(fileStream);
