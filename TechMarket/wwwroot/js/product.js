7a99c08aa4eb1f2a49c647af44589d4b0f9e159b
Umut Tokgöz <umttkgz@gmail.com>
2024-03-19 16:40:22 +0300

    Product Link in DataTable Edited

--- TechMarket/wwwroot/js/product.js
+++ TechMarket/wwwroot/js/product.js
@@ -7,9 +7,19 @@ function loadDataTable() {
         "ajax": { url: '/admin/product/getall' },
         "columns": [
             { data: 'title', "width": "25%" },
-            { data: 'price', "width": "10%" },
+            { data: 'price', "width": "15%" },
             { data: 'brand.name', "width": "20%" },
-            { data: 'subCategory.name', "width": "15%" }
+            { data: 'subCategory.name', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Düzenle</a>               
+                     <a class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Sil</a>
+                    </div>`
+                },
+                "width": "25%"
+            }
         ]
     });
 }
