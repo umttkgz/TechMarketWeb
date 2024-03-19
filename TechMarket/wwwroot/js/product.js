54800613ff52be2a5c8d39872f2076b870b5aa9c
Umut Tokgöz <umttkgz@gmail.com>
2024-03-19 16:32:18 +0300

    Load DataTables

---
+++ TechMarket/wwwroot/js/product.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/product/getall' },
+        "columns": [
+            { data: 'title', "width": "25%" },
+            { data: 'price', "width": "10%" },
+            { data: 'brand.name', "width": "20%" },
+            { data: 'subCategory.name', "width": "15%" }
+        ]
+    });
+}
